e1f38367ad38e090fa37b0e50a4de0db40cf8af9
Nicholas Chen <nchen@illinois.edu>
2012-01-12 16:08:26 -0600

    Added simplest working sequential case for PipelineCase0_b

---
+++ edu.illinois.flow.pipeline.cases/src/PipelineCase0_b.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PipelineCase0_b {
+	List<Item<Integer>> items = new ArrayList<Item<Integer>>();
+
+	void pipeline() {
+		System.out.println("Prologue");
+
+		for (Item<Integer> item : items) {
+			int temp1 = (Integer) item.getValue();
+			int temp2 = 2;
+			int a = temp1 + temp2;
+
+			int b = method1(a);
+
+			int c = method2(b);
+
+			method3(c);
+
+		}
+
+		System.out.println("Epilogue");
+	}
+
+	private void method3(int value) {
+		System.out.println(value);
+	}
+
+	private Integer method2(int value) {
+		return value + 2;
+	}
+
+	private Integer method1(int value) {
+		return value + 1;
+	}
+
+	public static void main(String[] args) {
+		PipelineCase0_b pipe = new PipelineCase0_b();
+
+		for (int i = 0; i < 100; i++)
+			pipe.items.add(new Item<Integer>(i));
+
+		pipe.pipeline();
+	}
+}
