32e074280dad4a515a90de0d6747c13a777026b9
Nicholas Chen <nchen@illinois.edu>
2012-01-12 14:54:02 -0600

    Inverted the loop and finished pipeline parallelism
    Instead of bundling everything neatly, we rely on having more explicit
    connections between the different variables and the uses.

--- edu.illinois.flow.pipeline.cases/src/PipelineCase3.java
+++ edu.illinois.flow.pipeline.cases/src/PipelineCase3.java
@@ -1,59 +1,81 @@
 import groovyx.gpars.DataflowMessagingRunnable;
+import groovyx.gpars.dataflow.Dataflow;
 import groovyx.gpars.dataflow.DataflowQueue;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PipelineCase3 {
 	List<Item<Integer>> items = new ArrayList<Item<Integer>>();
 
-	@SuppressWarnings("serial")
+	@SuppressWarnings({ "serial", "unchecked" })
 	void pipeline() throws InterruptedException {
 		System.out.println("Prologue");
 
+		final CountDownLatch latch = new CountDownLatch(1);
+
+		// These are the channels for communicating the item to each one
+		final DataflowQueue<Item<Integer>> channel0_a = new DataflowQueue<Item<Integer>>();
+		final DataflowQueue<Item<Integer>> channel0_b = new DataflowQueue<Item<Integer>>();
+		final DataflowQueue<Item<Integer>> channel0_c = new DataflowQueue<Item<Integer>>();
+
+		final DataflowQueue<Integer> channel1 = new DataflowQueue<Integer>();
+		final DataflowQueue<Integer> channel2 = new DataflowQueue<Integer>();
+		final DataflowQueue<Integer> channel3 = new DataflowQueue<Integer>();
+
+		Dataflow.operator(channel0_a, channel1,
+				new DataflowMessagingRunnable(1) {
+
+					@Override
+					protected void doRun(Object[] arguments) {
+						channel1.bind(method0((Item<Integer>) arguments[0]));
+					}
+				});
+
+		Dataflow.operator(channel0_b, channel2,
+				new DataflowMessagingRunnable(1) {
+
+					@Override
+					protected void doRun(Object[] arguments) {
+						channel2.bind(method1((Item<Integer>) arguments[0]));
+					}
+
+				});
+
+		Dataflow.operator(channel0_c, channel3,
+				new DataflowMessagingRunnable(1) {
+
+					@Override
+					protected void doRun(Object[] arguments) {
+						channel3.bind(method2((Item<Integer>) arguments[0]));
+					}
+				});
+
+		Dataflow.operator(Arrays.asList(channel1, channel2, channel3), null,
+				new DataflowMessagingRunnable(3) {
+
+					AtomicInteger counter = new AtomicInteger(100);
+
+					@Override
+					protected void doRun(Object[] arguments) {
+						method3((Integer) arguments[0], (Integer) arguments[1],
+								(Integer) arguments[2]);
+						counter.getAndDecrement();
+						if (counter.get() == 0)
+							latch.countDown();
+					}
+				});
+
 		for (Item<Integer> item : items) {
-			final DataflowQueue<Integer> channel1 = new DataflowQueue<Integer>();
-			new DataflowMessagingRunnable(1) {
-
-				@SuppressWarnings("unchecked")
-				@Override
-				protected void doRun(Object[] arguments) {
-					channel1.bind(method0((Item<Integer>) arguments[0]));
-				}
-			}.call(item);
-
-			final DataflowQueue<Integer> channel2 = new DataflowQueue<Integer>();
-			new DataflowMessagingRunnable(1) {
-
-				@SuppressWarnings("unchecked")
-				@Override
-				protected void doRun(Object[] arguments) {
-					channel2.bind(method1((Item<Integer>) arguments[0]));
-				}
-
-			}.call(item);
-
-			final DataflowQueue<Integer> channel3 = new DataflowQueue<Integer>();
-			new DataflowMessagingRunnable(1) {
-
-				@SuppressWarnings("unchecked")
-				@Override
-				protected void doRun(Object[] arguments) {
-					channel3.bind(method2((Item<Integer>) arguments[0]));
-				}
-			}.call(item);
-
-			new DataflowMessagingRunnable(3) {
-
-				@Override
-				protected void doRun(Object[] arguments) {
-					method3((Integer) arguments[0], (Integer) arguments[1],
-							(Integer) arguments[2]);
-				}
-			}.call(new Object[] { channel1.getVal(), channel2.getVal(),
-					channel3.getVal() });
-
+			channel0_a.bind(item);
+			channel0_b.bind(item);
+			channel0_c.bind(item);
 		}
+
+		latch.await();
 
 		System.out.println("Epilogue");
 	}
