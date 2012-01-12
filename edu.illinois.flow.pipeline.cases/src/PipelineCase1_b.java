d881f0408d50729a0119513dbf73679161f858b8
Nicholas Chen <nchen@illinois.edu>
2012-01-12 14:56:42 -0600

    Added simplest working sequential case for PipelineCase1_b
    This is a duplicate of PipelineCase1 except that I am going to attempt
    to do it without using the bundling technique.

---
+++ edu.illinois.flow.pipeline.cases/src/PipelineCase1_b.java
@@ -0,0 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class PipelineCase1_b {
+	List<Item<Integer>> items = new ArrayList<Item<Integer>>();
+
+	void pipeline() {
+		System.out.println("Prologue");
+
+		for (Item<Integer> item : items) {
+			int temp1 = (Integer) item.getValue();
+			int temp2 = 2;
+			int a = temp1 + temp2;
+
+			int b = method1(a);
+
+			int c = method2(b);
+
+			method3(a, b, c);
+
+		}
+
+		System.out.println("Epilogue");
+	}
+
+	private void method3(int value1, int value2, int value3) {
+		int i = value1 + value2 + value3;
+		System.out.println(i);
+	}
+
+	private int method2(int value) {
+		return value + 2;
+	}
+
+	private int method1(int value) {
+		return value + 1;
+	}
+
+	public static void main(String[] args) throws InterruptedException {
+		PipelineCase1_b pipe = new PipelineCase1_b();
+
+		for (int i = 0; i < 100; i++)
+			pipe.items.add(new Item<Integer>(i));
+
+		pipe.pipeline();
+	}
+}
