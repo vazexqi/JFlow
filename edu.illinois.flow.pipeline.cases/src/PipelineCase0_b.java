8290c726ae79c277d764bff84eed2ca620b2b262
Nicholas Chen <nchen@illinois.edu>
2012-01-12 16:17:36 -0600

    Inverted the loop and finished pipeline parallelism
    There is no explicit need to create a Token object here.

--- edu.illinois.flow.pipeline.cases/src/PipelineCase0_b.java
+++ edu.illinois.flow.pipeline.cases/src/PipelineCase0_b.java
@@ -1,8 +1,11 @@
 import groovyx.gpars.DataflowMessagingRunnable;
+import groovyx.gpars.dataflow.Dataflow;
 import groovyx.gpars.dataflow.DataflowQueue;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class PipelineCase0_b {
 	List<Item<Integer>> items = new ArrayList<Item<Integer>>();
@@ -11,48 +14,56 @@ public class PipelineCase0_b {
 	void pipeline() throws InterruptedException {
 		System.out.println("Prologue");
 
+		final CountDownLatch latch = new CountDownLatch(1);
+
+		final DataflowQueue<Item<Integer>> channel0 = new DataflowQueue<Item<Integer>>();
+		final DataflowQueue<Integer> channel1 = new DataflowQueue<Integer>();
+		final DataflowQueue<Integer> channel2 = new DataflowQueue<Integer>();
+		final DataflowQueue<Integer> channel3 = new DataflowQueue<Integer>();
+
+		Dataflow.operator(channel0, channel1, new DataflowMessagingRunnable(1) {
+
+			@SuppressWarnings("unchecked")
+			@Override
+			protected void doRun(Object[] arguments) {
+				channel1.bind(method0((Item<Integer>) arguments[0]));
+			}
+		});
+
+		Dataflow.operator(channel1, channel2, new DataflowMessagingRunnable(1) {
+
+			@Override
+			protected void doRun(Object[] arguments) {
+				channel2.bind(method1((Integer) arguments[0]));
+			}
+		});
+
+		Dataflow.operator(channel2, channel3, new DataflowMessagingRunnable(1) {
+
+			@Override
+			protected void doRun(Object[] arguments) {
+				channel3.bind(method2((Integer) arguments[0]));
+			}
+		});
+
+		Dataflow.operator(channel3, null, new DataflowMessagingRunnable(1) {
+
+			AtomicInteger counter = new AtomicInteger(100);
+
+			@Override
+			protected void doRun(Object[] arguments) {
+				method3((Integer) arguments[0]);
+				counter.getAndDecrement();
+				if (counter.get() == 0)
+					latch.countDown();
+			}
+		});
+
 		for (Item<Integer> item : items) {
-			final DataflowQueue<Integer> channel1 = new DataflowQueue<Integer>();
-			new DataflowMessagingRunnable(1) {
-
-				@SuppressWarnings("unchecked")
-				@Override
-				protected void doRun(Object[] arguments) {
-					channel1.bind(method0((Item<Integer>) arguments[0]));
-				}
-			}.call(item);
-			int a = channel1.getVal();
-
-			final DataflowQueue<Integer> channel2 = new DataflowQueue<Integer>();
-			new DataflowMessagingRunnable(1) {
-
-				@Override
-				protected void doRun(Object[] arguments) {
-					channel2.bind(method1((Integer) arguments[0]));
-				}
-			}.call(a);
-			int b = channel2.getVal();
-
-			final DataflowQueue<Integer> channel3 = new DataflowQueue<Integer>();
-			new DataflowMessagingRunnable(1) {
-
-				@Override
-				protected void doRun(Object[] arguments) {
-					channel3.bind(method2((Integer) arguments[0]));
-				}
-			}.call(b);
-			int c = channel3.getVal();
-
-			new DataflowMessagingRunnable(1) {
-
-				@Override
-				protected void doRun(Object[] arguments) {
-					method3((Integer) arguments[0]);
-
-				}
-			}.call(c);
-
+			channel0.bind(item);
 		}
+
+		latch.await();
 
 		System.out.println("Epilogue");
 	}
