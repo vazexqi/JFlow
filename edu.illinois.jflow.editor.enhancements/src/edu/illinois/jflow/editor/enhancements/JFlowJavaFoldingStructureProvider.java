da9eccd6f4a7cb918de32c34d61cf8515ad52732
Nicholas Chen <nchen@illinois.edu>
2012-05-09 18:08:25 -0500

    First version that can fold DataflowMessagingRunnable
    Go to Preferences > Java > Editor > Folding and select the JFlow Folding
    Support.
    
    There are several inherent limitations with the current implementation:
    
    1) Eclipse seems to only allow folding entire lines
    2) Eclipse seems to add funny ellipsis marks on each line
    3) There seems to be no way to change the textual presentation - it's
    pure line projection.

--- edu.illinois.jflow.editor.enhancements/src/edu/illinois/jflow/editor/enhancements/JFlowJavaFoldingStructureProvider.java
+++ edu.illinois.jflow.editor.enhancements/src/edu/illinois/jflow/editor/enhancements/JFlowJavaFoldingStructureProvider.java
@@ -7,21 +7,35 @@ package edu.illinois.jflow.editor.enhancements;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.eclipse.core.runtime.Assert;
 import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IMember;
 import org.eclipse.jdt.core.IMethod;
+import org.eclipse.jdt.core.ISourceRange;
 import org.eclipse.jdt.core.ISourceReference;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.SourceRange;
 import org.eclipse.jdt.ui.text.folding.DefaultJavaFoldingStructureProvider;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.jface.text.Position;
+import org.eclipse.jface.text.Region;
+import org.eclipse.jface.text.source.projection.IProjectionPosition;
 
 import edu.illinois.jflow.core.transformations.code.ExtractClosureRefactoring;
 
 /**
- * Provides some nicer folding for the DataflowMessagingRunnable class used in JFlow.
+ * Provides some nicer folding for the DataflowMessagingRunnable class used in JFlow. NOTE: A strong
+ * requirement for this class is that we reuse as much as possible from
+ * DefaultJavaFoldingStructureProvider *but* without changing any of its internals. This allows us
+ * to ship an unmodified copy of the JDT.
+ * 
+ * This class can be generalized to apply to all single method anonymous inner classes, e.g., lambda
  * 
  * @author Nicholas Chen
  */
@@ -30,7 +44,7 @@ public class JFlowJavaFoldingStructureProvider extends DefaultJavaFoldingStructu
 	@Override
 	protected void computeFoldingStructure(IJavaElement element, FoldingStructureComputationContext ctx) {
 		if (isDataflowMessagingRunnable(element)) {
-			computeJFLowFoldingStructure((IType)element, ctx);
+			computeJFlowFoldingStructure((IType)element, ctx);
 
 		} else if (isNestedDataflowMessagingRunnableMethod(element)) {
 			// If we have a IJavaElement.METHOD that is nested don't delegate to the superclass
@@ -71,7 +85,7 @@ public class JFlowJavaFoldingStructureProvider extends DefaultJavaFoldingStructu
 		return false;
 	}
 
-	private void computeJFLowFoldingStructure(IType element, FoldingStructureComputationContext ctx) {
+	private void computeJFlowFoldingStructure(IType element, FoldingStructureComputationContext ctx) {
 		IRegion[] regions= computeProjectionRanges((ISourceReference)element, ctx);
 		if (regions.length > 0) {
 			// comments
@@ -95,7 +109,7 @@ public class JFlowJavaFoldingStructureProvider extends DefaultJavaFoldingStructu
 			// code
 			IRegion normalized= alignRegion(regions[regions.length - 1], ctx);
 			if (normalized != null) {
-				Position position= element instanceof IMember ? createMemberPosition(normalized, (IMember)element) : createCommentPosition(normalized);
+				Position position= element instanceof IMember ? createJFlowClosurePosition(normalized, (IMember)element) : createCommentPosition(normalized);
 				if (position != null)
 					ctx.addProjectionRange(new JavaProjectionAnnotation(true, element, false), position);
 			}
@@ -109,14 +123,14 @@ public class JFlowJavaFoldingStructureProvider extends DefaultJavaFoldingStructu
 	////////////////////////////////////////////////////////////////////////////////////////////
 
 	/*
-	 * org.eclipse.jdt.ui.text.folding.DefaultJavaFoldingStructureProvider.isInnerType(IType)
+	 * @see org.eclipse.jdt.ui.text.folding.DefaultJavaFoldingStructureProvider.isInnerType(IType)
 	 */
 	private boolean isInnerType(IType type) {
 		return type.getDeclaringType() != null;
 	}
 
 	/*
-	 * See org.eclipse.jdt.ui.text.folding.DefaultJavaFoldingStructureProvider.isAnonymousEnum(IType)
+	 * @see org.eclipse.jdt.ui.text.folding.DefaultJavaFoldingStructureProvider.isAnonymousEnum(IType)
 	 */
 	private boolean isAnonymousEnum(IType type) {
 		try {
@@ -125,6 +139,7 @@ public class JFlowJavaFoldingStructureProvider extends DefaultJavaFoldingStructu
 			return false; // optimistically
 		}
 	}
+
 
 	// Use reflection to access some of the private methods in FoldingStructureComputationContext
 	private Object callPrivateContextMethod(FoldingStructureComputationContext ctx, String methodName) {
@@ -147,4 +162,139 @@ public class JFlowJavaFoldingStructureProvider extends DefaultJavaFoldingStructu
 		return null;
 	}
 
+	/*
+	 * @see org.eclipse.jdt.ui.text.folding.DefaultJavaFoldingStructureProvider.createMemberPosition(IRegion, IMember)
+	 */
+	protected Position createJFlowClosurePosition(IRegion aligned, IMember member) {
+		return new JFlowMemberPosition(aligned.getOffset(), aligned.getLength(), member);
+	}
+
+	/*
+	 * org.eclipse.jdt.ui.text.folding.DefaultJavaFoldingStructureProvider.JavaElementPosition
+	 */
+	private static final class JFlowMemberPosition extends Position implements IProjectionPosition {
+		private IMember fMember;
+
+		public JFlowMemberPosition(int offset, int length, IMember member) {
+			super(offset, length);
+			Assert.isNotNull(member);
+			fMember= member;
+		}
+
+		/*
+		 * @see org.eclipse.jface.text.source.projection.IProjectionPosition#computeFoldingRegions(org.eclipse.jface.text.IDocument)
+		 */
+		public IRegion[] computeProjectionRegions(IDocument document) throws BadLocationException {
+			int nameStart= offset;
+			try {
+				ISourceRange nameRange= fMember.getNameRange();
+				if (nameRange != null)
+					nameStart= nameRange.getOffset();
+
+			} catch (JavaModelException e) {
+				// ignore and use default
+			}
+
+			int firstLine= document.getLineOfOffset(offset);
+			int captionLine= document.getLineOfOffset(nameStart);
+			int lastLine= document.getLineOfOffset(offset + length);
+
+			if (captionLine < firstLine)
+				captionLine= firstLine;
+			if (captionLine > lastLine)
+				captionLine= lastLine;
+
+			IRegion preName= null;
+			IRegion preMethodRegion= null;
+			IRegion postMethodRegion= null;
+
+			if (firstLine < captionLine) {
+				int preOffset= document.getLineOffset(firstLine);
+				preName= new Region(preOffset, nameStart - preOffset);
+			} else {
+				preName= null;
+			}
+
+			if (captionLine < lastLine) {
+
+				//                             !! Show this region // Prename region
+				// someAnonymousClass() {      !! Show this region // CaptionLine
+				//                             !! Hide this region // Premethod region
+				//   void someMethod() {       !! Hide this region // Premethod region
+				//    ...                      !! Show this region
+				//    ...                      !! Show this region
+				//   }                         !! Hide this region // Postmethod region
+				//                             !! Hide this region // Postmethod region
+				// }                           !! Show this region
+
+				//TODO: Might be more robust to call the scanner so that we can account for different formatting styles
+
+				try {
+					IMethod doRunMethod; // There should only be one child
+					IJavaElement possibleMethod= fMember.getChildren()[0];
+					if (possibleMethod instanceof IMethod) {
+
+						doRunMethod= (IMethod)possibleMethod;
+						if (doRunMethod != null) {
+							ISourceRange methodNameRange= doRunMethod.getNameRange();
+							if (SourceRange.isAvailable(methodNameRange)) {
+								preMethodRegion= handlePreMethodRegion(document, captionLine, methodNameRange);
+							}
+
+							ISourceRange methodRange= doRunMethod.getSourceRange();
+							if (SourceRange.isAvailable(methodRange)) {
+								int postMethodOffset= methodRange.getOffset() + methodRange.getLength();
+								int lastLineOffset= document.getLineOffset(lastLine - 1);
+								postMethodRegion= new Region(postMethodOffset, lastLineOffset - postMethodOffset);
+							}
+						}
+					}
+				} catch (JavaModelException e) {
+					// If we have a JavaModelException it means that our methods are not well-formed yet. In that case, don't fold anything
+					return null;
+				}
+
+			}
+
+			List<IRegion> regions= new ArrayList<IRegion>();
+			if (preName != null)
+				regions.add(preName);
+			if (preMethodRegion != null)
+				regions.add(preMethodRegion);
+			if (postMethodRegion != null)
+				regions.add(postMethodRegion);
+
+			return regions.toArray(new IRegion[regions.size()]);
+		}
+
+		private IRegion handlePreMethodRegion(IDocument document, int captionLine, ISourceRange methodNameRange) throws BadLocationException {
+			IRegion preMethodRegion;
+			IRegion classInfo= document.getLineInformation(captionLine + 1);
+
+			int methodNameLine= document.getLineOfOffset(methodNameRange.getOffset());
+			int methodBodyLine= methodNameLine + 1; //TODO: This can be improved, the method body need not be the immediate next line
+
+			IRegion methodBodyInfo= document.getLineInformation(methodBodyLine);
+			int methodBodyOffset= methodBodyInfo.getOffset();
+			int classOffset= classInfo.getOffset();
+			preMethodRegion= new Region(classOffset, methodBodyOffset - classOffset);
+			return preMethodRegion;
+		}
+
+		/*
+		 * @see org.eclipse.jface.text.source.projection.IProjectionPosition#computeCaptionOffset(org.eclipse.jface.text.IDocument)
+		 */
+		public int computeCaptionOffset(IDocument document) throws BadLocationException {
+			int nameStart= offset;
+			try {
+				ISourceRange nameRange= fMember.getNameRange();
+				if (nameRange != null)
+					nameStart= nameRange.getOffset();
+			} catch (JavaModelException e) {
+			}
+
+			return nameStart - offset;
+		}
+
+	}
 }
