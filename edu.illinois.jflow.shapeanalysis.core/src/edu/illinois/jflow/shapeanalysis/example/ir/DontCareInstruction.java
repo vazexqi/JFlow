13caf2cea127c0c50df15d31c13eeace9a56f17e
Nicholas Chen <nchen@illinois.edu>
2013-02-08 15:34:56 -0600

    Add DontCareInstruction
    Needed for simple boolean/arithmetic instructions that do not manipulate
    the heap.
    
    See #3.

---
+++ edu.illinois.jflow.shapeanalysis.core/src/edu/illinois/jflow/shapeanalysis/example/ir/DontCareInstruction.java
@@ -0,0 +1,8 @@
+package edu.illinois.jflow.shapeanalysis.example.ir;
+
+/*
+ * Represents an instruction where we really do not care about its effects. This could
+ * be some arithmetic instruction or some boolean instruction in an actual program that does not manipulate the heap.
+ */
+public class DontCareInstruction extends FictionalIR {
+}
