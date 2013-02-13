f2d2d230b0e602088dd50c64c6576477413f0831
Nicholas Chen <nchen@illinois.edu>
2013-02-13 10:50:42 -0600

    Add transfer function for x := y
    See #2 and #10.

--- edu.illinois.jflow.shapeanalysis.core/src/edu/illinois/jflow/shapeanalysis/example/ir/AssignInstruction.java
+++ edu.illinois.jflow.shapeanalysis.core/src/edu/illinois/jflow/shapeanalysis/example/ir/AssignInstruction.java
@@ -1,7 +1,13 @@
 package edu.illinois.jflow.shapeanalysis.example.ir;
 
+import com.ibm.wala.fixpoint.UnaryOperator;
+
 import edu.illinois.jflow.shapenalaysis.shapegraph.structures.PointerVariable;
+import edu.illinois.jflow.shapenalaysis.shapegraph.structures.Selector;
+import edu.illinois.jflow.shapenalaysis.shapegraph.structures.SelectorEdge;
+import edu.illinois.jflow.shapenalaysis.shapegraph.structures.ShapeNode;
 import edu.illinois.jflow.shapenalaysis.shapegraph.structures.StaticShapeGraph;
+import edu.illinois.jflow.shapenalaysis.shapegraph.structures.VariableEdge;
 
 /*
  * x := y
@@ -16,4 +22,67 @@ public final class AssignInstruction extends FictionalIR<StaticShapeGraph> {
 	public String toString() {
 		return lhs + " := " + rhs;
 	}
+
+	@Override
+	public UnaryOperator<StaticShapeGraph> getTransferFunction() {
+		return this.new SSGAssign();
+	}
+
+	private final class SSGAssign extends UnaryOperator<StaticShapeGraph> {
+
+		@Override
+		public byte evaluate(StaticShapeGraph out, StaticShapeGraph in) {
+			StaticShapeGraph next= new StaticShapeGraph();
+
+			// VariablesEdges - Add new bindings for x
+			for (VariableEdge ve : in.getVariableEdges()) {
+				if (ve.n.containsName(getRhs())) {
+					PointerVariable x= new PointerVariable(getLhs());
+					next.addVariableEdge(new VariableEdge(x, ve.n.addNameIfContains(x, getRhs())));
+				}
+			}
+			// VariableEdges - Update old bindings for everything that y pointed to
+			for (VariableEdge ve : in.getVariableEdges()) {
+				PointerVariable x= new PointerVariable(getLhs());
+				next.addVariableEdge(new VariableEdge(new PointerVariable(ve.v), ve.n.addNameIfContains(x, getRhs())));
+			}
+
+			// SelectorEdges - Update everything that y pointed to also include x
+			for (SelectorEdge se : in.getSelectorEdges()) {
+				ShapeNode newSource= se.s.addNameIfContains(new PointerVariable(getLhs()), getRhs());
+				ShapeNode newTo= se.t.addNameIfContains(new PointerVariable(getLhs()), getRhs());
+				next.addSelectorEdge(new SelectorEdge(newSource, new Selector(se.sel), newTo));
+			}
+
+			// isShared
+			for (ShapeNode s : in.getIsShared().keySet()) {
+				Boolean resultofShapeNode= in.isShared(s.removeName(getLhs()));
+				next.getIsShared().put(new ShapeNode(s), resultofShapeNode);
+			}
+
+			if (!out.sameValue(next)) {
+				out.copyState(next);
+				return CHANGED;
+			} else {
+				return NOT_CHANGED;
+			}
+		}
+
+		@Override
+		public int hashCode() {
+			return "SSGAssign".hashCode();
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			return (o instanceof AssignInstruction.SSGAssign);
+		}
+
+		@Override
+		public String toString() {
+			return "StaticShapeGraph assign transfer function";
+		}
+
+	}
+
 }
