f463d64f1298ba806c9d9bb4cae304c0754a95bf
Nicholas Chen <nchen@illinois.edu>
2013-03-14 11:52:23 -0500

    Add skeleton for PDG unit tests
    See #30.

---
+++ edu.illinois.jflow.wala.core.ui.tests/src/edu/illinois/jflow/wala/core/ui/tests/PDGTests.java
@@ -0,0 +1,12 @@
+package edu.illinois.jflow.wala.core.ui.tests;
+
+import com.ibm.wala.cast.java.test.JDTJavaTest;
+import com.ibm.wala.ide.tests.util.EclipseTestUtil.ZippedProjectData;
+
+public class PDGTests extends JDTJavaTest {
+
+	public PDGTests(ZippedProjectData project) {
+		super(project);
+	}
+
+}
