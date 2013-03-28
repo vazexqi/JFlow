def40a534ac193d0462ef014db9c0b436876a1a4
Nicholas Chen <nchen@illinois.edu>
2013-03-28 15:41:49 -0500

    Create new separate plug-in project for ExtractClosure tests
    This makes the separation cleaner since this test needs the JDT UI.
    The previous one, i.e., edu.illinois.
    
    See #39.

---
+++ edu.illinois.jflow.core.transformations.ui.tests/src/edu/illinois/jflow/core/transformations/ui/tests/ExtractClosureTestSetup.java
@@ -0,0 +1,14 @@
+package edu.illinois.jflow.core.transformations.ui.tests;
+
+import junit.framework.Test;
+
+import org.eclipse.jdt.ui.tests.refactoring.RefactoringTestSetup;
+
+@SuppressWarnings("restriction")
+public class ExtractClosureTestSetup extends RefactoringTestSetup {
+
+	public ExtractClosureTestSetup(Test test) {
+		super(test);
+	}
+
+}
---
+++ edu.illinois.jflow.core.transformations.ui.tests/src/edu/illinois/jflow/core/transformations/ui/tests/ExtractClosureTests.java
@@ -0,0 +1,16 @@
+package edu.illinois.jflow.core.transformations.ui.tests;
+
+import org.eclipse.jdt.ui.tests.refactoring.AbstractSelectionTestCase;
+
+@SuppressWarnings("restriction")
+public class ExtractClosureTests extends AbstractSelectionTestCase {
+	private static ExtractClosureTestSetup fgTestSetup;
+
+	public ExtractClosureTests(String name) {
+		super(name);
+	}
+
+	public void testProject1() {
+
+	}
+}
