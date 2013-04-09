fa9b529045ed54375dbc05e4ab2a38bce6be934f
Nicholas Chen <nchen@illinois.edu>
2013-04-09 15:27:40 -0500

    Remove GPars from testing
    We don't really need it for analysis and somehow its inclusion
    (probably due to some internal main method) confuses WALA's pointer
    analysis(!!!).
    
    See #45.

---
+++ edu.illinois.jflow.wala.core.ui.tests/testdata/edu.illinois.jflow.test.data/src/partitionchecker/Project0.java
@@ -0,0 +1,12 @@
+package partitionchecker;
+
+public class Project0 {
+	Object field;
+
+	public static void main(String[] args) {
+		Project0 p1 = new Project0();
+		Project0 p2 = new Project0();
+		Object local1 = p1.field;
+		Object local2 = p2.field;
+	}
+}
