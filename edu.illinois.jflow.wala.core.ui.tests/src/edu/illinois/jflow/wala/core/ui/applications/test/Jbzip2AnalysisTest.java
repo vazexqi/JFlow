769734e2708af641313d0af436c5dbe6127d7ec8
Nicholas Chen <nchen@illinois.edu>
2013-04-21 10:29:24 -0500

    Add scalar dependencies test for jbzip2
    Found out quickly that one of the variables was being created outside
    the loop and was shared. It was easy to unshare it by creating a new one
    inside.
    
    See #54.

--- edu.illinois.jflow.wala.core.ui.tests/src/edu/illinois/jflow/wala/core/ui/applications/test/Jbzip2AnalysisTest.java
+++ edu.illinois.jflow.wala.core.ui.tests/src/edu/illinois/jflow/wala/core/ui/applications/test/Jbzip2AnalysisTest.java
@@ -1,5 +1,8 @@
 package edu.illinois.jflow.wala.core.ui.applications.test;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import java.io.IOException;
 import java.util.List;
 
@@ -10,6 +13,7 @@ import com.ibm.wala.shrikeCT.InvalidClassFileException;
 import com.ibm.wala.ssa.IR;
 import com.ibm.wala.util.CancelException;
 
+import edu.illinois.jflow.jflow.wala.dataflowanalysis.DataDependence;
 import edu.illinois.jflow.jflow.wala.dataflowanalysis.PDGPartitionerChecker;
 import edu.illinois.jflow.jflow.wala.dataflowanalysis.PipelineStage;
 import edu.illinois.jflow.jflow.wala.dataflowanalysis.ProgramDependenceGraph;
@@ -33,10 +37,35 @@ public class Jbzip2AnalysisTest extends JFlowTest {
 	}
 
 	@Test
-	public void testRoundTrip() throws IllegalArgumentException, IOException, CancelException, InvalidClassFileException {
+	public void testRoundTrip_ScalarDependencies() throws IllegalArgumentException, IOException, CancelException, InvalidClassFileException {
 		IR ir= retrieveMethodIR(constructFullyQualifiedClass(), "roundTrip", "Ljava/util/List;", "V");
 		ProgramDependenceGraph pdg= ProgramDependenceGraph.make(ir, engine.buildClassHierarchy());
-		List<List<Integer>> selections= selectionFromArray(new int[][] { { 96 }, { 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110 }, { 114, 115, 116, 117, 118, 119, 120, 121 } });
+		List<List<Integer>> selections= selectionFromArray(new int[][] { { 95 }, { 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110 }, { 114, 115, 116, 117, 118, 119, 120, 121 } });
+		PDGPartitionerChecker checker= PDGPartitionerChecker.makePartitionChecker(pdg, selections);
+
+		checker.computeHeapDependency(callGraph, engine.getPointerAnalysis());
+
+		PipelineStage stage1= checker.getStage(1);
+		List<DataDependence> stage1Input= stage1.getInputDataDependences();
+		assertEquals(1, stage1Input.size());
+		assertTrue(stage1Input.get(0).getLocalVariableNames().contains("inputFile"));
+		List<DataDependence> stage1Output= stage1.getOutputDataDependences();
+		assertEquals(1, stage1Output.size());
+		assertTrue(stage1Output.get(0).getLocalVariableNames().contains("tempFile"));
+
+		PipelineStage stage2= checker.getStage(2);
+		List<DataDependence> stage2Input= stage2.getInputDataDependences();
+		assertEquals(1, stage2Input.size());
+		assertTrue(stage2Input.get(0).getLocalVariableNames().contains("tempFile"));
+		List<DataDependence> stage2Output= stage2.getOutputDataDependences();
+		assertEquals(0, stage2Output.size());
+	}
+
+	@Test
+	public void testRoundTrip_HeapDependencies() throws IllegalArgumentException, IOException, CancelException, InvalidClassFileException {
+		IR ir= retrieveMethodIR(constructFullyQualifiedClass(), "roundTrip", "Ljava/util/List;", "V");
+		ProgramDependenceGraph pdg= ProgramDependenceGraph.make(ir, engine.buildClassHierarchy());
+		List<List<Integer>> selections= selectionFromArray(new int[][] { { 95 }, { 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110 }, { 114, 115, 116, 117, 118, 119, 120, 121 } });
 		PDGPartitionerChecker checker= PDGPartitionerChecker.makePartitionChecker(pdg, selections);
 
 		checker.computeHeapDependency(callGraph, engine.getPointerAnalysis());
