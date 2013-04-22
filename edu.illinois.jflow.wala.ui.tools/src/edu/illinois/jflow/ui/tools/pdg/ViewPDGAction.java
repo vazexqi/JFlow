e70e85cbea3f9ebc3e4f06d73ea930bbdc44c754
Nicholas Chen <nchen@illinois.edu>
2013-04-22 15:50:57 -0500

    Remove testing code
    We had some old testing code here that we can now safely remove.
    
    See #56.

--- edu.illinois.jflow.wala.ui.tools/src/edu/illinois/jflow/ui/tools/pdg/ViewPDGAction.java
+++ edu.illinois.jflow.wala.ui.tools/src/edu/illinois/jflow/ui/tools/pdg/ViewPDGAction.java
@@ -1,8 +1,6 @@
 package edu.illinois.jflow.ui.tools.pdg;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Set;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.ICompilationUnit;
@@ -11,12 +9,9 @@ import org.eclipse.jdt.internal.ui.actions.SelectionConverter;
 import org.eclipse.jdt.internal.ui.javaeditor.JavaEditor;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.ITextSelection;
 
 import com.ibm.wala.shrikeCT.InvalidClassFileException;
 
-import edu.illinois.jflow.jflow.wala.dataflowanalysis.DataDependence;
-import edu.illinois.jflow.jflow.wala.dataflowanalysis.PDGExtractClosureAnalyzer;
 import edu.illinois.jflow.jflow.wala.dataflowanalysis.ProgramDependenceGraph;
 import edu.illinois.jflow.wala.ui.tools.graph.jdt.util.JavaEditorUtil;
 
@@ -41,17 +36,6 @@ public class ViewPDGAction extends Action {
 				view.setDocument(document);
 				view.setPDG(graph);
 				view.updateGraph(graph);
-
-				// For testing
-				ITextSelection selection= (ITextSelection)javaEditor.getSelectionProvider().getSelection();
-				PDGExtractClosureAnalyzer analyzer= new PDGExtractClosureAnalyzer(graph, document, selection.getOffset(), selection.getLength());
-
-				analyzer.analyzeSelection();
-				List<DataDependence> inputDataDependences= analyzer.getInputDataDependences();
-				List<DataDependence> outputDataDependences= analyzer.getOutputDataDependences();
-				Set<String> closureLocalVariableNames= analyzer.getClosureLocalVariableNames();
-				System.out.println("Done");
-
 			} catch (IOException e) {
 				e.printStackTrace();
 			} catch (CoreException e) {
