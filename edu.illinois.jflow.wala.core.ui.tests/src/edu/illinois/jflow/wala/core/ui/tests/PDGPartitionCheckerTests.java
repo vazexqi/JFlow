ae51873ee756bec4e246c38481c6cc3552eed0ef
Nicholas Chen <nchen@illinois.edu>
2013-06-13 15:07:14 -0500

    Update the test cases

--- edu.illinois.jflow.wala.core.ui.tests/src/edu/illinois/jflow/wala/core/ui/tests/PDGPartitionCheckerTests.java
+++ edu.illinois.jflow.wala.core.ui.tests/src/edu/illinois/jflow/wala/core/ui/tests/PDGPartitionCheckerTests.java
@@ -452,6 +452,17 @@ public class PDGPartitionCheckerTests extends JFlowTest {
 		assertTrue(checker.hasInterference());
 	}
 
+	@Test
+	public void testProject12_checkHeapAnalysis() throws IOException, InvalidClassFileException, CancelException {
+		IR ir= retrieveMethodIR(constructFullyQualifiedClass(), "main", "[Ljava/lang/String;", "V");
+		ProgramDependenceGraph pdg= ProgramDependenceGraph.make(ir, engine.buildClassHierarchy());
+		List<List<Integer>> selections= selectionFromArray(new int[][] { { 5 }, { 7, 8 }, { 12 } });
+		PDGPartitionerChecker checker= PDGPartitionerChecker.makePartitionChecker(pdg, selections);
+		checker.computeHeapDependency(callGraph, engine.getPointerAnalysis());
+		checker.checkInterference();
+		assertFalse(checker.hasInterference());
+	}
+
 	// This is a test to confirm that even for simple totally independent stages, we have trouble for files
 	@Test
 	public void testFileAccess_checkHeapAnalysis() throws IOException, InvalidClassFileException, CancelException {
